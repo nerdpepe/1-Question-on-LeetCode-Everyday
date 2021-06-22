c3cee30e0d94b3c1cd849ccae95e5a2a8959a2ce
Aditya Bhardwaj <aditbhardwj@gmail.com>
2021-06-23 01:20:51 +0530

    Short solution with Queue

---
+++ Array/Duplicate Zeroes/DuplicateZeroes_Queue.java
@@ -0,0 +1,24 @@
+package Array;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class DuplicateZeroes_Queue {
+    static void duplicateZeros(int[] arr) {
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            queue.add(arr[i]);
+            // push additional 0 if arr[i] was zero
+            if (arr[i] == 0)
+                queue.add(0);
+
+            arr[i] = queue.poll();
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[] { 1, 0, 2, 3, 0, 4, 5, 0 };
+        duplicateZeros(arr);
+    }
+}
