786bdcf199d7cebc13b5af91325924d57c223fbd
Aditya Bhardwaj <aditbhardwj@gmail.com>
2021-06-25 06:42:52 +0530

    Removing Elements - questions

---
+++ Array/RemoveElements/RemoveDuplicatesSortedArr.java
@@ -0,0 +1,15 @@
+package RemoveElements;
+
+public class RemoveDuplicatesSortedArr {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length < 2)
+            return nums.length;
+
+        int k = 1;
+        for (int i = 1; i < nums.length; i++)
+            if (nums[i] != nums[i - 1])
+                nums[k++] = nums[i];
+
+        return k;
+    }
+}
