f5b5314666412c54f181b825e0680f420e8e9baf
Aditya Bhardwaj <aditbhardwj@gmail.com>
2021-07-01 17:48:21 +0530

    Find start of loop in LinkedList

---
+++ LinkedList/LoopNodeLinkedList.java
@@ -0,0 +1,34 @@
+package LinkedList;
+
+//import java.util.HashSet;
+
+public class LoopNodeLinkedList {
+    // public ListNode detectCycle(ListNode head) {
+    // HashSet<ListNode> hs = new HashSet<>();
+    // while (head!=null && head.next!=null) {
+    // if(!hs.add(head))
+    // return head;
+    // head = head.next;
+    // }
+    // return null;
+    // }
+
+    public ListNode detectCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                fast = head;
+                while (fast != slow) {
+                    fast = fast.next;
+                    slow = slow.next;
+                }
+
+                return fast;
+            }
+        }
+        return null;
+    }
+}
