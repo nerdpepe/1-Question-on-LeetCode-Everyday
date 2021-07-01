1f158413ddc285730744dde738da247db53b242c
Aditya Bhardwaj <aditbhardwj@gmail.com>
2021-07-01 15:18:53 +0530

    Two pointer approach

--- LinkedList/CycleLinkedList.java
+++ LinkedList/CycleLinkedList.java
@@ -11,13 +11,37 @@ class ListNode {
 }
 
 public class CycleLinkedList {
+
+    /*
+     * Traversing each node and changing the val which can later be checked if there
+     * is a cycle
+     */
+
+    // public boolean hasCycle(ListNode head) {
+    // while (head != null && head.next != null) {
+    // if (head.val == 1000000)
+    // return true;
+    // head.val = 1000000;
+    // head = head.next;
+    // }
+    // return false;
+    // }
+
+    /*
+     * Using two-pointer approach. One fast and other slow. They will come together
+     * if there is a cycle
+     */
+
     public boolean hasCycle(ListNode head) {
-        while (head != null && head.next != null) {
-            if (head.val == 1000000)
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow)
                 return true;
-            head.val = 1000000;
-            head = head.next;
         }
         return false;
     }
+
 }
