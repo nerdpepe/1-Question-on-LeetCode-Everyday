37e9f0e4c2899f9e365cd65f0b284fa546995f31
Aditya Bhardwaj <aditbhardwj@gmail.com>
2021-07-04 12:13:17 +0530

    Remove Nth Node from the End - other way

--- LinkedList/NthNodeRemoval.java
+++ LinkedList/NthNodeRemoval.java
@@ -15,22 +15,28 @@ public class NthNodeRemoval {
 
         // edge conditions
 
-        // only one element
+        // only one element in the LinkedList
         if (i == 1) {
             head = null;
             return head;
         }
 
-        // if n is first or last
+        // if n is 1, remove the last element
         if (n == 1) {
             ListNode node = hm.get(i - 1);
             node.next = node.next.next;
             return head;
         }
+
+        // if n is equal to i (length), remove first element
         if (n == i) {
             head = head.next;
             return head;
         }
 
+        // others
+        ListNode node = hm.get(i - n);
+        node.next = node.next.next;
+        return head;
     }
 }
---
+++ LinkedList/NthNodeRemoval_other.java
@@ -0,0 +1,36 @@
+package LinkedList;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class NthNodeRemoval_other {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode temp = new ListNode(0, head);
+        ListNode one = temp;
+        ListNode two = temp;
+        for (int i = 1; i <= n; i++)
+            two = two.next;
+
+        while (two.next != null) {
+            one = one.next;
+            two = two.next;
+        }
+
+        one.next = one.next.next;
+        return temp.next;
+    }
+}
