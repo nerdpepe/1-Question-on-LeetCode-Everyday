c86216b852d4ff3a01825530046346b35915623a
Aditya Bhardwaj <aditbhardwj@gmail.com>
2021-07-05 09:26:21 +0530

    Reversing Linked List

--- LinkedList/ClassicProblems/ReverseLinkedList.java
+++ LinkedList/ClassicProblems/ReverseLinkedList.java
@@ -1,5 +1,36 @@
 package LinkedList.ClassicProblems;
 
-public class ReverseLinkedList {
+class ListNode {
+    int val;
+    ListNode next;
 
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class ReverseLinkedList {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+
+        ListNode h = head;
+        ListNode p = head.next;
+
+        while (p != null) {
+            ListNode temp = new ListNode(p.val, head);
+            h.next = p.next;
+            p = p.next;
+            head = temp;
+        }
+        return head;
+    }
 }
