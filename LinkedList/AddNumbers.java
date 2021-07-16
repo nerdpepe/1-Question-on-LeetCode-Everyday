2d4fbfb9f543a33b817e245bb09507509efd7e4b
Aditya Bhardwaj <aditbhardwj@gmail.com>
2021-07-16 18:17:57 +0530

    Add numbers in the 2 Linked Lists - v2

--- LinkedList/AddNumbers.java
+++ LinkedList/AddNumbers.java
@@ -4,47 +4,28 @@
  * ListNode next) { this.val = val; this.next = next; } }
  */
 public class AddNumbers {
-    static int maxLen = 0;
-
-    static int[] getNumber(ListNode l) {
-        int[] arr = new int[101];
-        int exp = 0;
-        while (l != null) {
-            arr[exp++] = l.val;
-            l = l.next;
-        }
-        maxLen = (maxLen < exp) ? exp : maxLen;
-        return arr;
-    }
-
-    static ListNode getLinkedList(int len, int[] m, int[] n) {
-        int i = 0;
-        int carry = 0;
-        int sum = 0;
-
-        ListNode l = new ListNode(0, null);
-        ListNode head = l;
-
-        while (m[i] != 0 || n[i] != 0 || carry != 0 || i < len) {
-            sum = carry + m[i] + n[i];
-            l.next = new ListNode(sum % 10);
-            carry = sum / 10;
-            l = l.next;
-            i++;
-        }
-        return (head.next == null) ? head : head.next;
-    }
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        int[] n_1;
-        int[] n_2;
-        maxLen = 0;
+        ListNode head = new ListNode(0);
+        ListNode point = head;
+        int sum = 0, carry = 0;
+        while (l1 != null || l2 != null) {
+            int a = (l1 == null) ? 0 : l1.val;
+            int b = (l2 == null) ? 0 : l2.val;
+            sum = a + b + carry;
+            point.next = new ListNode(sum % 10);
+            carry = sum / 10;
+            point = point.next;
 
-        // process the 2 linked lists and get numbers
-        n_1 = getNumber(l1);
-        n_2 = getNumber(l2);
+            if (l1 != null)
+                l1 = l1.next;
+            if (l2 != null)
+                l2 = l2.next;
+        }
 
-        // convert the number to linked list and return
-        return getLinkedList(maxLen, n_1, n_2);
+        if (carry > 0)
+            point.next = new ListNode(carry);
+
+        return head.next;
     }
 }
